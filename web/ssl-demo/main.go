fe6350df218e839f6d6ce61a4e6676c6237627eb
Avinash Reddy Desireddy <avinashr.desireddy@gmail.com>
2020-09-08 16:21:42 -0500

    SSL demo

---
+++ web/ssl-demo/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    // "fmt"
+    "net/http"
+    "log"
+)
+
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+    w.Header().Set("Content-Type", "text/plain")
+    w.Write([]byte("SSL Server running on port :443\n"))
+}
+
+func main() {
+
+    http.HandleFunc("/", HelloServer)
+    http.HandleFunc("/hello", HelloServer)
+    err := http.ListenAndServeTLS(":443", "server.crt", "server.key", nil)
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
+}
